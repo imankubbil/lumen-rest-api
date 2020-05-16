243a96c63553b6ebbcf8c19cbf138987944c5884
imankubbil <imankubbil@gmail.com>
2020-05-17 03:34:20 +0700

    fresh

---
+++ app/Produk.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+  protected $table = 'produk';
+
+    protected $fillable = [
+        'nama', 'harga', 'warna',
+        'kondisi', 'deskripsi'
+    ];
+}
